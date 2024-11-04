bc5ca7cb210728e456341bc2af09fbf85ae3befb
vood <igor-vood@gmail.com>
2024-11-04 18:46:26 +0300

    init

--- vood-dictionary/src/main/resources/liquibase/pdd/0030.pdd_constraint.sql
+++ vood-dictionary/src/main/resources/liquibase/pdd/0030.pdd_constraint.sql
@@ -6,14 +6,13 @@ SELECT
     tc.constraint_type,
     kcu.column_name,
     kcu.ordinal_position,
-    ccu.table_schema parent_table_schema,
-    ccu.table_name parent_table_name,
-    ccu.column_name parent_column_name
+    rc.unique_constraint_schema,
+    rc.unique_constraint_name
 FROM information_schema.table_constraints tc
-         join information_schema.key_column_usage kcu on
+         left join information_schema.key_column_usage kcu on
     (tc.constraint_name, tc.table_schema, tc.table_name) = ((kcu.constraint_name, kcu.table_schema, kcu.table_name))
-         join information_schema.constraint_column_usage ccu on
-    (ccu.constraint_name, ccu.table_schema, ccu.table_name, ccu.column_name) = ((tc.constraint_name, tc.table_schema, tc.table_name, kcu.column_name))
+         left join information_schema.referential_constraints rc on
+    (rc.constraint_name, rc.constraint_schema) = ((tc.constraint_name, tc.constraint_schema))
 /
 COMMENT ON VIEW pdd_constraint IS 'Метаинформация колонок таблиц базы'
 /
@@ -25,11 +24,9 @@ COMMENT ON COLUMN pdd_constraint.column_name IS 'Имя колонки'
 /
 COMMENT ON COLUMN pdd_constraint.ordinal_position IS 'Позиция колонки в таблице'
 /
-COMMENT ON COLUMN pdd_constraint.parent_table_schema IS 'Родитель. Наименование схемы таблицы'
+COMMENT ON COLUMN pdd_constraint.unique_constraint_schema IS 'Схема UK куда ссылается форен'
 /
-COMMENT ON COLUMN pdd_constraint.parent_table_name IS 'Родитель. Имя таблицы'
-/
-COMMENT ON COLUMN pdd_constraint.parent_column_name IS 'Родитель. Имя колонки'
+COMMENT ON COLUMN pdd_constraint.unique_constraint_name IS 'наименование UK куда ссылается форен'
 /
 COMMENT ON COLUMN pdd_constraint.constraint_type IS 'Тип констрайнта'
 /
