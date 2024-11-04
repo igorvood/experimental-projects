fe0bfd63c300354ebf7247f63de0eb3dd5fdbcf0
vood <igor-vood@gmail.com>
2024-11-04 18:25:16 +0300

    init

--- vood-dictionary/src/main/resources/liquibase/stored/0010.dict_meta.sql
+++ vood-dictionary/src/main/resources/liquibase/stored/0010.dict_meta.sql
@@ -1,13 +1,20 @@
 create table dict_meta
 (
   id varchar(256) not null,
-  description varchar(512),
-  constraint dict_meta_pkk primary key (id, description)
+  is_deleted  boolean not null,
+  description varchar(512) not null,
+--   deleted_id varchar(256) generated always as (
+--     case when is_deleted = true then id end
+--   ),
+  constraint dict_meta_pk primary key (id)
+--   constraint dict_meta_uk unique (deleted_id)
 )
 /
 comment on table dict_meta is 'Мета информация по справочникам.'
 /
 comment on column dict_meta.id is 'Идентификатор справочника.'
+/
+comment on column dict_meta.is_deleted is 'Признак удаленного справочника.'
 /
 comment on column dict_meta.description is 'Описание.'
 /
