7f96240034dbf17a473cf747e3531e7e06962038
vood <igor-vood@gmail.com>
2024-11-04 12:04:24 +0300

    init

---
+++ vood-dictionary/src/main/resources/liquibase/release_001/task_001/graph_structure/0118.dict_service.sql
@@ -0,0 +1,14 @@
+create table dict_service
+(
+  id varchar(255) not null,
+  main_class varchar(255) not null,
+  constraint dict_service_pk primary key (id),
+  node_type varchar(256) generated always as ('flink_srv') stored,
+)
+/
+comment on table dict_service is 'Справочник групп настроек для консьюмера топиков.'
+/
+comment on column dict_service.id is 'Идентификатор сервиса.'
+/
+comment on column dict_service.main_class is 'Класс запуска.'
+/
